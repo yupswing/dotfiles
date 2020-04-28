ce2b201ea660da3b3960d7bee67e48ae686b209b
Simone Cingano <yupswing@protonmail.com>
2020-04-28 18:09:45 +0200

    to kill pulse and relatives in vm

--- dotfiles/zsh/zshlib/modules/Linux/65_arch.zsh
+++ dotfiles/zsh/zshlib/modules/Linux/65_arch.zsh
@@ -1,4 +1,13 @@
 if [[ $(uname -a) =~ "arch" ]]; then
   alias y=yay
   alias pac=pacman
+
+  function pulseagain() {
+    pulseaudio -k && pulseaudio --start && /home/simo/.config/polybar/launch.zsh
+    \killall -v Discord
+    \killall -v spotify
+    sleep 2
+    discord &
+    spotify &
+  }
 fi
