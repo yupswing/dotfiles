b9a7c0610a21b0ea4eaac6bbbe3beb1a69feefb4
Simone Cingano <yupswing@protonmail.com>
2020-07-24 18:14:32 +0200

    :lipstick: newline if too long (server zsh)

--- server/zsh/base.zsh
+++ server/zsh/base.zsh
@@ -1,5 +1,6 @@
 # yup server zsh config 1.0
 
-PROMPT='%F{green}%n%f@%F{magenta}%m%f %F{blue}%B%~%b%f %# '
+
+PROMPT="%F{green}%n%f@%F{magenta}%m%f %F{blue}%B%"$'~%-30(l::\n)'"%b%f %# "
 RPROMPT='[%F{yellow}%?%f]'
 alias update_zshrc=wget --no-cache https://raw.githubusercontent.com/yupswing/dotfiles/master/server/zsh/update.zsh -O /tmp/update.zsh && chmod +x /tmp/update.zsh && /tmp/update.zsh
