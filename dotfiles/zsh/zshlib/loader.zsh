78616d229559140a586e0777cb0a49b7e67fc115
Simone Cingano <yupswing@protonmail.com>
2020-10-16 16:31:55 +0200

    arch maintenance and per-host variables

--- dotfiles/zsh/zshlib/loader.zsh
+++ dotfiles/zsh/zshlib/loader.zsh
@@ -1,3 +1,6 @@
+# ============================================================================
+# Loading function
+
 function loadRC() {
   # Load all .zsh in a folder
   if [ -d $1 ]; then
@@ -8,18 +11,32 @@ function loadRC() {
   unset zshFile
 }
 
-
+# ============================================================================
 # Load main library
 loadRC $YUPZSHRC_MODULES
 
+# ============================================================================
 # Load platform library (Darwin, Linux...)
 loadRC "$YUPZSHRC_MODULES/$(uname -s)"
 
+# ============================================================================
+# Load hostname library
+hostnameDir="$YUPZSHRC_MODULES/Hostname/$(cat /proc/sys/kernel/hostname)"
+if [ -d $hostnameDir ]; then
+  loadRC $hostnameDir
+fi
+
+# ============================================================================
 # Local (per Host) customizations
 for localDir in ~ /etc/zsh /etc /usr/local/etc /usr/local/etc/zsh; do
   if [ -f ${localDir}/zshrc.local ]; then
     source ${localDir}/zshrc.local
   fi
 done
+
+# ============================================================================
+# Unset temporary variables and functions
+
+unset hostnameDir
 unset localDir
 unset loadRC
