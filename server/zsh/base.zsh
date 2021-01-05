0b69eb95e715f71304556e87ff4b98bb19097858
Simone Cingano <yupswing@protonmail.com>
2021-01-05 10:53:04 +0100

    add new key

--- server/zsh/base.zsh
+++ server/zsh/base.zsh
@@ -4,6 +4,9 @@
 PROMPT="%F{green}%n%f@%F{magenta}%m%f %F{blue}%B%"$'~%-30(l::\n)'"%b%f %# "
 RPROMPT='[%F{yellow}%?%f]'
 
+export EDITOR='nvim'
+export VISUAL=$EDITOR
+
 # Updater
 alias update_zshrc="wget --no-cache --no-http-keep-alive https://raw.githubusercontent.com/yupswing/dotfiles/master/server/zsh/update.zsh -O /tmp/update.zsh && chmod +x /tmp/update.zsh && /tmp/update.zsh && rm /tmp/update.zsh"
 
