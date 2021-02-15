03f71e08eed34cbff12b3ff22fa89123e698cb94
Simone Cingano <yupswing@protonmail.com>
2021-02-15 18:01:01 +0100

    locale en-GB

--- dotfiles/zsh/zshenv.zsh
+++ dotfiles/zsh/zshenv.zsh
@@ -2,8 +2,8 @@
 # ================================== ZSHENV ==================================
 # ----------------------------------------------------------------------------
 
-export LANGUAGE='en_US.UTF-8'
-export LANG='en_US.UTF-8'
+export LANGUAGE='en_GB.UTF-8'
+export LANG='en_GB.UTF-8'
 export LC_ALL='en_GB.UTF-8'
 export TZ='Europe/Rome'
 
