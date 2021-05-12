9aa400606322baddf23e1c8cf216d476fc141a8f
Simone Cingano <yupswing@protonmail.com>
2021-05-12 16:52:41 +0200

    alignment after pyenv changes

--- dotfiles/zsh/zshlib/modules/40_environments.zsh
+++ dotfiles/zsh/zshlib/modules/40_environments.zsh
@@ -43,6 +43,7 @@ fi
 export PYENV_ROOT="$HOME/.pyenv"
 if [[ -d $PYENV_ROOT ]]; then
   export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
   function pyenv_list() {
     echo " # pyenv install -l\n # filtered to show only last python versions\n"
