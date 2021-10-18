c4908ae57047e0444cd353e0555b7106987820ab
Simone Cingano <yupswing@protonmail.com>
2021-10-18 15:14:28 +0200

    clean up rofi menu

--- dotfiles/scripts/rofi-power.zsh
+++ dotfiles/scripts/rofi-power.zsh
@@ -1,49 +1,31 @@
 #!/usr/bin/env zsh
-# Original script by Oliver Kraitschy - http://okraits.de
-# Modified by Simone Cingano @yupswing
 
-OPTIONS=" Power-off system
- Reboot system
- End session
- Lock screen
- Suspend system
- Hibernate system"
 LOCKER=$HOME/.scripts/lock.zsh
 
-LAUNCHER="rofi -dmenu -i -p  -width -30"
-option=$(echo -e "$OPTIONS" | ${(s: :)LAUNCHER} | awk '{print $2}' | tr -d '\r\n')
+# Commands to execute in an ordered array which will be used as a sort of hash:
+MENU=(
+  ' Power-off system'    'systemctl poweroff'
+  ' Reboot system'       'systemctl reboot'
+  ' End session'         '$HOME/.config/bspwm/scripts/quit.sh'
+  ' Lock screen'         '$LOCKER'
+  ' Suspend system'      'systemctl suspend; $LOCKER'
+  # ' Hibernate system'    'systemctl hibernate; $LOCKER'
+)
 
-if [ ${#option} -gt 0 ]; then
-  case $option in
-  Power-off)
-    systemctl poweroff
-    ;;
-  Reboot)
-    systemctl reboot
-    ;;
-  End)
-    case ${GDMSESSION:l} in
-      i3)
-        i3-msg exit
-        ;;
-      bspwm)
-        $HOME/.config/bspwm/scripts/quit.sh
-        # bspc quit
-        ;;
-    esac
-    ;;
-  Lock)
-    eval $LOCKER
-    ;;
-  Suspend)
-    systemctl suspend
-    eval $LOCKER
-    ;;
-  Hibernate)
-    systemctl hibernate
-    eval $LOCKER
-    ;;
-  *) ;;
+# Extract menu labels and menu commands
+MENU_LABELS=()
+MENU_COMMANDS=()
+for ((index = 1; index <= $#MENU; index++)); do
+  (( index % 2 )) && MENU_LABELS+=("${MENU[index]}")
+  (( index % 2 )) || MENU_COMMANDS+=("${MENU[index]}")
+done
 
-  esac
-fi
+# Launch rofi with labels
+LAUNCHER="rofi -dmenu -i -p  -width -30 -format i"
+SELECTED=$(printf "%s\n" "${MENU_LABELS[@]}" | ${(s: :)LAUNCHER})
+
+# No selection
+[[ -z $SELECTED ]] && exit
+
+# Selected an index (-format i), but it is zerobased
+eval $MENU_COMMANDS[SELECTED+1]
