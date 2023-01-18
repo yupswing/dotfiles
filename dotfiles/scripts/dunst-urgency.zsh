6134957c56ca1e7d9a071b1ec2a2801c80d6347c
Simone Cingano <me@simonecingano.it>
2023-01-18 18:02:20 +0100

    dunst

--- dotfiles/scripts/dunst-urgency.zsh
+++ dotfiles/scripts/dunst-urgency.zsh
@@ -1,9 +1,18 @@
 #!/usr/bin/env zsh
 
-[[ -z $DUNST_APP_NAME ]] && exit
+[[ -z $DUNST_DESKTOP_ENTRY ]] && exit
 
+# # WM INDEPENDENT SET URGENT (to be tested)
+# Get lowest numeric id (is it a wid? it does not look like it) and make it urgent
+# (lowest because it is probably the main window)
+# (It does not work with Thunderbird, the main window is a random one in the list)
+# xdotool set_window --urgency 1 \
+#     "$(xdotool search --name "${DUNST_DESKTOP_ENTRY}" | sort -n | head -n1)" \
+#      &> /dev/null
+
+# BSPWM SET URGENT
 # Get all WID related to appname (from actual shown app in bspc)
-wid=$(comm -12 <(bspc query -N | sort) <(xdo id -N $DUNST_APP_NAME | sort))
+wid=$(comm -12 <(bspc query -N | sort) <(xdo id -N $DUNST_DESKTOP_ENTRY | sort))
 [[ -z $wid ]] && exit
 
 # Get last WID (if more than one)
